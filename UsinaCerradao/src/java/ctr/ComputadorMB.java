445acb24fcb171deee6a51f8df47b78906611e7f
enascimento <enascimento@MICRO103.usinacerradao.com.br>
2019-01-15 12:26:19 -0200

    Criado o metodo de busca do fornecedor e numero danf em computador.

--- UsinaCerradao/src/java/ctr/ComputadorMB.java
+++ UsinaCerradao/src/java/ctr/ComputadorMB.java
@@ -7,7 +7,12 @@ package ctr;
 
 import dao.Dao;
 import java.io.Serializable;
+import java.math.BigDecimal;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
@@ -15,6 +20,8 @@ import javax.faces.event.ActionEvent;
 import javax.faces.model.SelectItem;
 import model.Computador;
 import model.Marca;
+import model.NotaFiscal;
+
 import util.FacesUtil;
 
 /**
@@ -26,15 +33,20 @@ import util.FacesUtil;
 public class ComputadorMB implements Serializable {
     
     private Computador computador;
+    private NotaFiscal notaFiscal;
      private Dao dao = new Dao();
      private List<SelectItem> selectMarca; 
     private List<Marca> listaMarca;
+    private List<NotaFiscal>listaNf;
     List<Computador> listarComputador= new ArrayList<Computador>();
+    private Date teste;
 
     public ComputadorMB() {
         computador = new  Computador();
+        notaFiscal = new  NotaFiscal();
         computador.setMarca(new Marca());
         listarComputador= new ArrayList<Computador>();
+        listaNf = new ArrayList<NotaFiscal>();
         listarComputador = (List<Computador>) dao.buscarTodos(Computador.class);
     }
     public void gravar (ActionEvent evt)
@@ -59,6 +71,34 @@ public List<SelectItem> getSelectMarca() {
             selectMarca.add(new SelectItem(c.getIdMarca(), c.getDescricao()));
         }
         return selectMarca;
+    }
+
+public void buscarNf() throws ParseException {
+    DateFormat fmt = new SimpleDateFormat("dd/MM/yyyy");
+    String sData = fmt.format(computador.getData());
+    
+    //computador = new  Computador();
+    System.out.println("-------------------------------" + computador.getNf());
+    System.out.println("-------------------------------" + sData);
+    Date data = fmt.parse(sData);
+    List<Object[]> results = dao.buscarNfPorCodigo(computador.getNf(),sData);
+        NotaFiscal nf = null;
+        for (Object[] result : results) {
+            
+            nf = new NotaFiscal();
+            nf.setNumero((BigDecimal) result[0]);
+            nf.setSerie((String) result[1]);
+            nf.setData((Date) result[2]);
+            nf.setRazaoSocial((String) result[3]);
+            nf.setDanf((String) result[4]);
+            nf.setCdProduto((String) result[5]);
+            nf.setDescrProduto((String) result[6]);
+            getListaNf().add(nf);
+            computador.setFornecedor(nf.getRazaoSocial());
+        computador.setNroDanf(nf.getDanf());
+        }
+        
+       
     }
     public Computador getComputador() {
         return computador;
@@ -94,6 +134,30 @@ public List<SelectItem> getSelectMarca() {
 
     public void setSelectMarca(List<SelectItem> selectMarca) {
         this.selectMarca = selectMarca;
+    }
+
+    public List<NotaFiscal> getListaNf() {
+        return listaNf;
+    }
+
+    public void setListaNf(List<NotaFiscal> listaNf) {
+        this.listaNf = listaNf;
+    }
+
+    public NotaFiscal getNotaFiscal() {
+        return notaFiscal;
+    }
+
+    public void setNotaFiscal(NotaFiscal notaFiscal) {
+        this.notaFiscal = notaFiscal;
+    }
+
+    public Date getTeste() {
+        return teste;
+    }
+
+    public void setTeste(Date teste) {
+        this.teste = teste;
     }
     
     
--- UsinaCerradao/src/java/dao/Dao.java
+++ UsinaCerradao/src/java/dao/Dao.java
@@ -2,6 +2,7 @@ package dao;
 
 import java.io.Serializable;
 import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -63,11 +64,16 @@ public class Dao implements Serializable {
     }
 
     public List<Object[]> buscarNF() {
-        TypedQuery<Object[]> query = (TypedQuery<Object[]>) em.createNativeQuery("SELECT A.NRO,a.SERIE,a.DT_EMISS,a.RAZAO_SOCIAL,a.NRO_NFE,b.CD_PROD,b.DESCR_PROD FROM NF_ENT a, ITNF_ENT b WHERE a.NFENT_ID = b.NFENT_ID AND b.CD_PROD  in ('54578','54579','54160','54164') order by a.DT_EMISS");
+        //TypedQuery<Object[]> query = (TypedQuery<Object[]>) em.createNativeQuery("SELECT A.NRO,a.SERIE,a.DT_EMISS,a.RAZAO_SOCIAL,a.NRO_NFE,b.CD_PROD,b.DESCR_PROD FROM NF_ENT a, ITNF_ENT b WHERE a.NFENT_ID = b.NFENT_ID AND b.CD_PROD  in ('54578','54579','54160','54164') order by a.DT_EMISS");
+        TypedQuery<Object[]> query = (TypedQuery<Object[]>) em.createNativeQuery("SELECT A.NRO,a.SERIE,a.DT_EMISS,a.RAZAO_SOCIAL,a.NRO_NFE,b.CD_PROD,b.DESCR_PROD FROM NF_ENT a, ITNF_ENT b WHERE a.NFENT_ID = b.NFENT_ID AND b.CD_PROD  in ('54578','54579','54160','54164') and a.nro = 575842 and a.DT_EMISS = '27/04/2012' order by a.DT_EMISS");
         List<Object[]> results = query.getResultList();
         return results;
     }
-
+    public List<Object[]> buscarNfPorCodigo(int nf, String data) {
+        TypedQuery<Object[]> query = (TypedQuery<Object[]>) em.createNativeQuery("SELECT A.NRO,a.SERIE,a.DT_EMISS,a.RAZAO_SOCIAL,a.NRO_NFE,b.CD_PROD,b.DESCR_PROD FROM NF_ENT a, ITNF_ENT b WHERE a.NFENT_ID = b.NFENT_ID AND b.CD_PROD  in ('54578','54579','54160','54164') and a.nro = "+nf+" and a.DT_EMISS  = '"+data+"' order by a.DT_EMISS");
+        List<Object[]> results = query.getResultList();
+        return results;
+    }
     public List<Object[]> buscarColaboradores(BigDecimal mat) {
         TypedQuery<Object[]> query = (TypedQuery<Object[]>) em.createNativeQuery("select nome_colab, cd_colab from v_colab where dt_demis is null and cd_colab = " + mat);
         List<Object[]> results = query.getResultList();

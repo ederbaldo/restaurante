9e2196e2e2bb092d38dc39334e63e3ea19842381
Jarciano Silva <jsilva@ajotta.com>
2019-03-15 15:51:25 -0300

    Tentativa de corrigir o menu

--- UsinaCerradao/web/resources/usinati/javascripts/app.js
+++ UsinaCerradao/web/resources/usinati/javascripts/app.js
@@ -1,58 +1,58 @@
-$(function() {
-	$('.js-toggle').bind('click', function(event) {
-		$('.js-sidebar, .js-content').toggleClass('is-toggled');
-		event.preventDefault();
-	});
+$(function () {
+    $('.js-toggle').bind('click', function (event) {
+        $('.js-sidebar, .js-content').toggleClass('is-toggled');
+        event.preventDefault();
+    });
 
-	$('.aw-item-link').bind('click', function(event) {
-		debugger;
-		event.preventDefault();
-		expandirRecolherItensDoMenu(event);
-	});
+    $('.aw-item-link').bind('click', function (event) {
+        debugger;
+        event.preventDefault();
+        expandirRecolherItensDoMenu(event);
+    });
 });
 
 function expandirRecolherItensDoMenu(event) {
 
-	debugger;
+    debugger;
 
-	var linkClicado = event.target;
+    var linkClicado = event.target;
 
-	// se clicou no icone do link em vez de clicar no link
-	if ($(linkClicado).hasClass('fa-fw')) {
-		linkClicado = $(linkClicado).parent();
-	}
+    // se clicou no icone do link em vez de clicar no link
+    if ($(linkClicado).hasClass('fa-fw')) {
+        linkClicado = $(linkClicado).parent();
+    }
 
-	avaliarQualLiRecolherExpandir($(linkClicado).parent(), linkClicado);
+    avaliarQualLiRecolherExpandir($(linkClicado).parent(), linkClicado);
 }
 
 function avaliarQualLiRecolherExpandir(liPai, linkClicado) {
 
-	// quando for subitem nao precisa fazer nada
-	if (!($(liPai).hasClass('subitem'))) {
+    // quando for subitem nao precisa fazer nada
+    if (!($(liPai).hasClass('subitem'))) {
 
-		// se nao estiver selecionada, desativa a li que estiver selecionda
-		if (!($(liPai).hasClass('is-selected'))) {
-			desativarLiSelecionada();
-		}
-		expandirRecolherMenu(linkClicado, liPai);
-	}
+        // se nao estiver selecionada, desativa a li que estiver selecionda
+        if (!($(liPai).hasClass('is-selected'))) {
+            desativarLiSelecionada();
+        }
+        expandirRecolherMenu(linkClicado, liPai);
+    }
 }
 
 function expandirRecolherMenu(linkClicado, liPai) {
 
-	$(liPai).toggleClass('is-selected');
-	var proximoMenu = $(linkClicado).next('.aw-menu-tree');
-	proximoMenu.toggle('1000');
+    $(liPai).toggleClass('is-selected');
+    var proximoMenu = $(linkClicado).next('.aw-menu-tree');
+    proximoMenu.toggle('1000');
 }
 
 function desativarLiSelecionada() {
 
-	var liAtiva = $('.is-selected');
+    var liAtiva = $('.is-selected');
 
-	if (liAtiva.length > 0) {
-		var menuExpandido = $(liAtiva).find('.aw-menu-tree');
-		menuExpandido.toggle('1000');
-		$(liAtiva).removeClass('is-selected');
-               
-	}
+    if (liAtiva.length > 0) {
+        var menuExpandido = $(liAtiva).find('.aw-menu-tree');
+        menuExpandido.toggle('1000');
+        $(liAtiva).removeClass('is-selected');
+
+    }
 }
